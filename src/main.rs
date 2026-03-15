a89693b47f7b281a9c0b90d92213f24fcc98d558
SorerBOT <alonfiller@gmail.com>
2026-03-16 01:42:40 +0200

    FEAT removed deps

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
 use rusty_ytdl::Video;
 
 #[tokio::main]
-async fn main() {
-    println!("Downloader is starting up!");
-    
-    // We will put the download logic right here!
+async fn main()
+{
+    std::println!()
 }
