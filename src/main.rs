c89f2d8b29d04f46900b8d07e3d0f86e4c3a2e0d
SorerBOT <alonfiller@gmail.com>
2026-03-16 02:13:42 +0200

    FEAT now able to get a JSON dump

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,27 @@
-use rusty_ytdl::Video;
+use std::process::Command;
 
-#[tokio::main]
-async fn main()
+fn download_video(url: &str) -> Result<(),Box<dyn std::error::Error>>
 {
-    std::println!()
+    let output = Command::new("yt-dlp")
+        .arg("--dump-json")
+        .arg("-f")
+        .arg("best[ext=mp4]")
+        .arg(url)
+        .output()?;
+
+    let json_content = String::from_utf8(output.stdout)?;
+
+    print!("{}", json_content);
+
+
+
+    Ok(())
+}
+
+fn main()
+{
+    let url = "https://www.youtube.com/watch?v=NV401gLmiAk";
+
+    download_video(url)
+        .expect("Failed to download video.");
 }
